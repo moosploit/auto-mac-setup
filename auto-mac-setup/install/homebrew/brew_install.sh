14a2421befe81b80b6e1d6ad95bfdd2caf1cc753
Michael Linder <12547698+moosploit@users.noreply.github.com>
2020-08-28 23:44:58 +0200

    Added file header

--- auto-mac-setup/install/homebrew/brew_install.sh
+++ auto-mac-setup/install/homebrew/brew_install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
-
-
+# ================================================================================
+# -- File:          homebrew/brew_install.sh
+# -- Project:       auto-mac-setup
+# -- Project URL:   https://github.com/moosploit/auto-mac-setup
+# -- Create Date:   2020-08-28 13:07
+# -- Author:        Michael Linder
+# -- Company:       moosploit.com
+# -- License:       MIT License | http://www.opensource.org/licenses/MIT
+# ================================================================================
