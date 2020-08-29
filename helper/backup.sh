535d2049ca28bb38f1caa7ef899afe96449b50bb
Michael Linder <12547698+moosploit@users.noreply.github.com>
2020-08-29 13:04:06 +0200

    Added some backup functions

--- helper/backup.sh
+++ helper/backup.sh
@@ -9,10 +9,25 @@
 # -- License:       MIT License | http://www.opensource.org/licenses/MIT
 # ================================================================================
 
-# Backup Homebrew
-bkp_brew_list() {
+# Backup homebrew
+function bkp_homebrew() {
+    brew_bkp_folder=$HOME"/backup/install/homebrew"
+    mkdir -p $brew_bkp_folder
+
+    brew leaves > $brew_bkp_folder"/brew-list.txt"
+    brew cask list -1 > $brew_bkp_folder"/brew-cask-list.txt"
 }
 
-# Backup MAS
-bkp_mas_list() {
+# Backup Mac App Store
+function bkp_mas() { 
+    mas_bkp_folder=$HOME"/backup/install/mas"
+    mkdir -p $mas_bkp_folder
+
+    mas list > $mas_bkp_folder"/mas-list.txt"
+}
+
+# Backup current dotfiles
+#TODO bring the function to work
+function bkp_dofiles() {
+    mkdir -p ~/backup/dotfiles
 }
