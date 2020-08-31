b25103bf78c549ee93c845ded92b51f532b1a7e6
Michael Linder <12547698+moosploit@users.noreply.github.com>
2020-08-31 20:22:49 +0200

    Initialized setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ================================================================================
+# -- File:          auto-mac-setup/setup.sh
+# -- Project:       auto-mac-setup
+# -- Project URL:   https://github.com/moosploit/auto-mac-setup
+# -- Create Date:   2020-08-28 11:58
+# -- Author:        Michael Linder
+# -- Company:       moosploit.com
+# -- License:       MIT License | http://www.opensource.org/licenses/MIT
+# ================================================================================
+
+source ./helper/output.sh
+
+cat hello
