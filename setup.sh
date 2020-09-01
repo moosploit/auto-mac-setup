ee417e0e35b6a15957dd2bfdb658b92604455926
Michael Linder <12547698+moosploit@users.noreply.github.com>
2020-09-02 00:03:56 +0200

    Added the first lines

--- setup.sh
+++ setup.sh
@@ -11,3 +11,14 @@
 
 source ./helper/utils.sh
 source ./helper/output.sh
+
+print_cat "Meow there! I will help you to auto configure your new Apple Mac."
+ask_for_confirmation "Shall we procced?"
+
+if ! answer_is_yes; then
+    print_cat "OK! Hope you'll come back soon."
+    exit
+fi
+
+print_cat "Purrrrrrfect! - Let us begin. May i have your password please?"
+ask_for_sudo
