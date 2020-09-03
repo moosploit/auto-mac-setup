37537db82ea12da96b1bfd5b652bad1f2983a6d4
Michael Linder <12547698+moosploit@users.noreply.github.com>
2020-09-03 22:52:34 +0200

    Updated some of the util functions in utils.sh

--- helper/utils.sh
+++ helper/utils.sh
@@ -9,54 +9,87 @@
 # -- License:       MIT License | http://www.opensource.org/licenses/MIT
 # ================================================================================
 
+# Check whether the script is called directly or via source
+$(return &>/dev/null)
+
+if [ "$?" -ne "0" ]; then
+	source ./output.sh
+fi
+
 check_cmd_exists() {
-    command -v "$1" &> /dev/null
+	command -v "$1" &>/dev/null
+}
+
+check_already_sudo() {
+	sudo -n true 2>/dev/null
 }
 
 ask_for_sudo() {
-    sudo -v
+	if ! check_already_sudo; then
+		print_cat "May i have your password please?"
 
-    # https://gist.github.com/cowboy/3118588
-    while true; do
-        sudo -n true
-        sleep 60
-        kill -0 "$$" || exit
-    done &> /dev/null &
+		sudo -v
+
+		# https://gist.github.com/cowboy/3118588
+		while true; do
+			sudo -n true
+			sleep 60
+			kill -0 "$$" || exit
+		done &>/dev/null &
+	else
+		print_cat "You have already sudo privileges!"
+	fi
 }
 
 ask_for_confirmation() {
-    print_question "$1 (y|N) "
-    read -r -p "          "
+	print_question "$1 (y|N) "
+	read -r -p "          "
 }
 
 answer_is_yes() {
-    [[ "$REPLY" =~(y|Y) ]] && return 0 || return 1
+	[[ "$REPLY" =~ (y|Y) ]] && return 0 || return 1
+}
+
+execude_cmd() {
+	local -r CMD="$1"
+	local -r MSG="$2"
+
+	local exitCode=0
+	local cmdPID=""
+
+	echo ${MSG}
 }
 
 get_os() {
-    local os=""
-    local kernelName="$(uname -s)"
+	local os=""
+	local kernelName="$(uname -s)"
 
-    if [ "$kernelName" == "Darwin" ]; then
-        os="macos"
-    elif [ "$kernelName" == "Linux" ] && [ -e "/etc/os-release" ]; then
-        os="$(. /etc/os-release; printf "%s" "$ID")"
-    else
-        os="$kernelName"
-    fi
+	if [ "$kernelName" == "Darwin" ]; then
+		os="macos"
+	elif [ "$kernelName" == "Linux" ] && [ -e "/etc/os-release" ]; then
+		os="$(
+			. /etc/os-release
+			printf "%s" "$ID"
+		)"
+	else
+		os="$kernelName"
+	fi
 
-    printf "%s" "$os"
+	printf "%s" "$os"
 }
 
 get_os_version() {
-    local version=""
-    local os="$(get_os)"
+	local version=""
+	local os="$(get_os)"
 
-    if [ "$os" == "macos" ]; then
-        version="$(sw_vers -productVersion)"
-    elif [ -e "/etc/os-release" ]; then
-        version="$(. /etc/os-release; printf "%s" "$VERSION_ID")"
-    fi
+	if [ "$os" == "macos" ]; then
+		version="$(sw_vers -productVersion)"
+	elif [ -e "/etc/os-release" ]; then
+		version="$(
+			. /etc/os-release
+			printf "%s" "$VERSION_ID"
+		)"
+	fi
 
-    printf "%s" "$version"
+	printf "%s" "$version"
 }
