934a727a5aed1c43a295e5850718b6a82e519530
Michael Linder <12547698+moosploit@users.noreply.github.com>
2020-09-11 22:11:13 +0200

    Added function 'computer_name_setup'

--- src/config/macos/setup_macos.sh
+++ src/config/macos/setup_macos.sh
@@ -16,3 +16,40 @@ if [ "$?" -ne "0" ]; then
 	source ../../../helper/output.sh
 	source ../../../helper/utils.sh
 fi
+
+computer_name_setup() {
+
+	local MAC_NAME=""
+
+	ask_for_confirmation "Should I setup your computer name?"
+	if ! answer_is_yes; then
+		return 0
+	fi
+
+	while [[ -z $MAC_NAME ]]; do
+		ask_for_input "How should I name your new Apple Mac?"
+		MAC_NAME=$REPLY
+	done
+
+	scutil_set "ComputerName" "$MAC_NAME"
+	scutil_set "HostName" "$MAC_NAME"
+	scutil_set "LocalHostName" "$MAC_NAME"
+	defaults_write "Apple SMB Server" "/Library/Preferences/SystemConfiguration/com.apple.smb.server" "NetBIOSName" "string" "$MAC_NAME"
+}
+
+dock_setup() {
+	:
+}
+
+__init__() {
+	ask_for_sudo
+
+	# Close all System Preference panes to prevent them to overriding our settings
+	osascript -e 'tell application "System Preferences" to quit'
+
+	print_cat "First I will setup some general settings for your Apple Mac!"
+	computer_name_setup
+
+}
+
+__init__
