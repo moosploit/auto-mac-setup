4c8953984322c95fa9625aeebed768b832cbf29d
Michael Linder <12547698+moosploit@users.noreply.github.com>
2020-09-17 13:48:13 +0200

    Initial commit of 'setup_moom.sh'

---
+++ src/config/moom/setup_moom.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ================================================================================
+# -- File:          moom/setup_moom.sh
+# -- Project:       auto-mac-setup
+# -- Project URL:   https://github.com/moosploit/auto-mac-setup
+# -- Create Date:   2020-09-16 21:39
+# -- Author:        Michael Linder
+# -- Company:       moosploit.com
+# -- License:       MIT License | http://www.opensource.org/licenses/MIT
+# ================================================================================
+
+# Check whether the script is called directly or via source
+if [[ $(basename ${0}) == $(basename ${BASH_SOURCE}) ]]; then
+	source ../../../helper/output.sh
+	source ../../../helper/utils.sh
+fi
+
+
+
+
+__init__() {
+
+}
+
+__init__
